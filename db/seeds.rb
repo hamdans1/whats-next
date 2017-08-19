1235272e2eac4f354643c1d28e39c06082c89b45
Sami Hamdan <samihamdan00@gmail.com>
2017-08-19 00:17:27 +0000

    seeded using seinfeld quotes from Faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+50.times do
+    User.create!(
+        email: Faker::Internet.unique.email,
+        password: Faker::Internet.password(7)
+        )
+end
+
+users = User.all
+
+25.times do
+    Item.create!(
+        user: users.sample,
+        name: Faker::Seinfeld.quote
+        )
+end
+
+items = Item.all
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{Item.count} items created"
