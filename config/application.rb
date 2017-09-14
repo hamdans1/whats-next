85e9765cabad7cd6fe7805beb6c2a05558160947
Sami Hamdan <samihamdan00@gmail.com>
2017-09-14 01:59:52 +0000

    whitelisted ips fix

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module Workspace
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     
-    config.web_console.whitelisted_ips = '98.7.37.218'
+    config.web_console.whitelisted_ips = '192.168.1.5'
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
